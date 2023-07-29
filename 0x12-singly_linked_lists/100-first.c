4fca52c6dbae79454ed02a26bdc6193fbd489f07
curry-bean <curry.bean.017@gmail.com>
2023-07-29 04:56:08 +0300

    Hare&Tortoise2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,6 +3,6 @@
 /* This is the constructor function */
 void __attribute__ ((constructor)) bmain()
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
