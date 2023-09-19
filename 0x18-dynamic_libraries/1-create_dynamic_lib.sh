589631b58a98fbf73724017e9330ec9abdf78692
spaceadh <spacesealadhesives13@gmail.com>
2023-09-19 05:06:15 +0300

    TrialRun2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-gcc -Wall -fPIC -c *.c
-gcc -shared -o liball.so *.o
+# #!/bin/bash
+# gcc -Wall -fPIC -c *.c
+# gcc -shared -o liball.so *.o
 
+
+#!/bin/bash
+gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
