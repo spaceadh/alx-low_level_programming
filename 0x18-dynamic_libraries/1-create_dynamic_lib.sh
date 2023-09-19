b7a7fc809bc57846585333601dece4628609e74b
spaceadh <spacesealadhesives13@gmail.com>
2023-09-19 05:16:01 +0300

    Asubuhi hii yote ??

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,3 @@
-# #!/bin/bash
-# gcc -Wall -fPIC -c *.c
-# gcc -shared -o liball.so *.o
-
-
 #!/bin/bash
-gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
+gcc -c -fpic *.c
+gcc -shared -o liball.so *.o
