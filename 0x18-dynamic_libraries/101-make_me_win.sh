08696d1c1ff0019ef3b53d9d47c4c772fa7209c9
spaceadh <spacesealadhesives13@gmail.com>
2023-09-19 05:20:18 +0300

    nrandom.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/issar13/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/spaceadh/alx-low_level_programming/raw/main/0x18-dynamic_libraries/nrandom.so
+#wget -P /tmp/ https://raw.github.com/issar13/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so[putshack.so[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[putshack.so[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
