2bcb43c78a68e165cb7311135a61e4b6b54ee330
spaceadh <spacesealadhesives13@gmail.com>
2024-01-15 11:54:04 +0300

    FinishedWithDeclaringFiles

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -22,6 +22,7 @@ typedef struct listint_s
 } listint_t;
 
 
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
