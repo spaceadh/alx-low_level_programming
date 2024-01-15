c3d33b329d4268f91b152d875c3c8a98e2973b1b
spaceadh <spacesealadhesives13@gmail.com>
2024-01-15 08:02:46 +0300

    FinishedNumber1

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,29 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
+int linear_search(int *array, size_t size, int value);
+
+
+
+#endif
